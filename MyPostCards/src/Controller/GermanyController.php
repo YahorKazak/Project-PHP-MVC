5bc2a8f1d5a51367e416bc3cc86ad05be91ba3f4
YahorKazak <50490788+YahorKazak@users.noreply.github.com>
2020-06-17 19:49:09 +0200

    Database Image Done

---
+++ MyPostCards/src/Controller/GermanyController.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Germany;
+use App\Migrations\ProductType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use App\Service\FileUploader;
+
+
+class GermanyController extends AbstractController
+{
+    /**
+     * @Route("/image/Germany", name="app_product_newG")
+     * @param Request $request
+     * @param SluggerInterface $slugger
+     * @param FileUploader $fileUploader
+     * @return RedirectResponse|Response
+     */
+
+    public function new(Request $request, SluggerInterface $slugger,FileUploader $fileUploader)
+    {
+        $postcard = new Germany();
+        $form = $this->createForm(ProductType::class, $postcard);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $brochureFile */
+            $brochureFile = $form['brochure']->getData();
+            if ($brochureFile) {
+                $brochureFileName = $fileUploader->upload($brochureFile);
+                $postcard->setBrochureFilename($brochureFileName);
+                $entityManager = $this->getDoctrine()->getManager();
+                $postcard ->setImage($brochureFile);
+                $entityManager->persist($postcard);
+                $entityManager->flush();
+            }
+
+            // ...
+        }
+
+        return $this->render('Image/Germany.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
+}
